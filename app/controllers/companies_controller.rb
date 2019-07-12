b4bc5978c0ee1bb2384b889b9543d64012af6eee
Lucy Suddenly <suddenlylucy@gmail.com>
2019-07-12 10:25:20 -0700

    fixed cors, added search action to companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,10 @@
 class CompaniesController < ApplicationController
 
     def search
-        
+        company = Company.find_by(name: params["searchForm"][0].downcase)
+        if company == nil 
+            company = {none: "none"}
+        end
+        render json: company
     end
 end
