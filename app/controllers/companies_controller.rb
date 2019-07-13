122e5cf9ba9e640433c56e7eefe31fd2b3c9ee7e
Lucy Suddenly <suddenlylucy@gmail.com>
2019-07-13 15:22:44 -0700

    added create action to companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,4 +8,20 @@ class CompaniesController < ApplicationController
         end
         render json: result
     end
+
+    def create
+        company = Company.new(company_params)
+        if company.valid?
+            company.save
+            render json: company
+        else
+            render json: {message: }
+        end 
+    end
+
+    private
+
+    def company_params
+        params.permit(:name, :website, :image_url, :description)
+    end
 end
