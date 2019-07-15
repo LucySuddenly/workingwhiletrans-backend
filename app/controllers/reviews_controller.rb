4bdcfeb2700ea3135da4054d483a0388c9d3a949
Lucy Suddenly <suddenlylucy@gmail.com>
2019-07-15 14:07:03 -0700

    fixed create action on reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+
+    def create 
+        review = Review.new(review_params)
+        if review.valid?
+            review.save
+            render json: review
+        else
+            render json: {"message": "the review was not successfully saved"}
+        end
+    end
+
+    private 
+
+    def review_params
+        params.require(:review).permit(:title, :job_title, :body, :rating, :company_id)
+    end
 end
