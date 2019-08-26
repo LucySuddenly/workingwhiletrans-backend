4527afb2655d283a9edd524bd7f5d98ec6b0f6fe
Lucy Suddenly <suddenlylucy@gmail.com>
2019-08-26 09:19:20 -0700

    added model validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,8 @@
 class Company < ApplicationRecord
     has_many :reviews
+    validates :name, uniqueness: true
+    validates :name, presence: true 
+    validates :description, presence: true
+    validates :image_url, presence: true 
+    validates :website, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
     belongs_to :company
+    validates :title, presence: true
+    validates :job_title, presence: true
+    validates :body, presence: true 
+    validates :rating, presence: true
 end
